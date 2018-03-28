0651ebc33a40f5f28e0bf74c51736bbb502063cf
Seiryuu26 <davidphotiade@hotmail.fr>
2018-03-28 09:47:42 +0300

    Add files via upload

--- model/AdminManager.php
+++ model/AdminManager.php
@@ -1,11 +1,11 @@
 <?php
-namespace www\p3\Model;
+namespace www\p3\model;
 require_once("model/Manager.php");
 
 class AdminManager extends Manager
  {
  
-   public function connectedWhere($pseudo,$motdepasse)
+   public function connected ($pseudo,$motdepasse)
    {
      $db= $this->dbConnect();
      $req = $db->prepare('SELECT id,nom,prenom,pseudo,motdepasse FROM users WHERE pseudo=:pseudo AND motdepasse=:motdepasse');
