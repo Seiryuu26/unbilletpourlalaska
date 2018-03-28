a82ef926a114c3ea86267b45641e2c10550ff25a
Seiryuu26 <davidphotiade@hotmail.fr>
2018-03-28 09:42:19 +0300

    Add files via upload
    modify form action as =?action=connexion

--- view/frontend/connectView.php
+++ view/frontend/connectView.php
@@ -1,16 +1,17 @@
 <?php 
 session_start();?>
-    <div id="login" class="span3 well well-large offset4">
+    <div id="espace d'administration" class="span3 well well-large offset4">
         <div class="centreConnect">
             <h4>Connexion</h4>
             <br>
-            <form action="admin.php?action=connexion" class="form" method="post">
-                <input type="text" placeholder="Pseudo" name="pseudo" />
-                <br>
-                <br>
-                <input type="password" placeholder="Password" name="pass" required/>
-                <br>
-                <br>
-                <input class="btn btn-success" type="submit" value="Login" required /> </form>
+            <form action="?action=connexion" class="form" method="post" />
+                <p>
+                <input type="text" placeholder="Pseudo" name="pseudo" value="" />
+                </p>
+                <p>
+                <input type="password" placeholder="Password" name="motdepasse" value="" required/>
+                </p>
+                <p>
+                <input class="btn btn-success" type="submit" value="Login" required /></p> </form>
         </div>
     </div>
