7ab227a146592fe71060b6f3b8e3994a7f72962a
Seiryuu26 <davidphotiade@hotmail.fr>
2019-05-27 21:33:31 +0200

    update  for the most recent version of the website

--- index.php
+++ index.php
@@ -20,7 +20,6 @@ try {
                 $frontoffice->listPosts();
             } elseif ($_GET['action'] == 'post') {
                 if (isset($_GET['id']) && $_GET['id'] > 0) {
-                    $frontoffice->post();
                 } else {
                     throw new Exception ('Any identifiant de billet send');
                 }
@@ -37,7 +36,7 @@ try {
             } elseif ($_GET['action'] == 'login') {
 
                 if (empty($_SESSION['pseudo']) OR empty($_SESSION['id'])) {
-                    $login = $backoffice->login();// Appel d'une fonction de cet objet
+                    $login = $backoffice->login();// Appel d'une fonction de cet objeT
                 } else header('Location: index.php?action=board');
 
             } else if ($_GET['action'] == 'connexion') {
@@ -61,19 +60,11 @@ try {
              * look for the method name to match the action (method board)
              */
             elseif ($_GET['action'] == 'signalComment') {
-
-
                 $frontoffice->signalComment($_GET['id']);
-
             } elseif (!empty($_SESSION['pseudo']) && !empty($_SESSION['id'])) {
-
                 if ($_GET['action'] == 'logout') {
-
-
                     $backoffice->logout();
-
                 } elseif ($_GET['action'] == 'addPost') {
-
 
                     // $postchapitre, $titre, $contenu
                     if (!empty($_POST['title']) && !empty($_POST['content'])) {
@@ -82,45 +73,26 @@ try {
                         throw new Exception (' Error of submission !');
                     }
                 } elseif ($_GET['action'] == 'erasePost') {
-
-
                     $backoffice->erasePost($_GET['id']);
-
                 } elseif ($_GET['action'] == 'modifyPost') {
-
                     if (!empty($_POST['title']) && !empty($_POST['content'])) {
                         $backoffice->domodifyPost($_POST);
                     } else {
                         //Call to function to display the form
                         $backoffice->modifyPost($_GET['id']);
                     }
-
-
                 } elseif ($_GET['action'] == 'board') {
-
-
                     $backoffice->board();
-
                 } elseif ($_GET['action'] == 'logout') {
-
-
                     $backoffice->logout();
-
                 } elseif ($_GET['action'] == 'signalComment') {
-
-
                     $frontoffice->signalComment($_GET['id']);
-
                 } elseif ($_GET['action'] == 'eraseComment') {
-
                     $backoffice->eraseComment($_GET['id']);
-
                 }
             } elseif ($_GET['action'] == 'board')
-
                 header('Location: index.php?action=login');
                }
-
 else {
         $frontoffice->listPosts();
 }
