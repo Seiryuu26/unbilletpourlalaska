9ae6b8ecf363eb88545bef945a848d049121dd06
littleBlackMan <littleblackman@users.noreply.github.com>
2019-06-06 12:58:04 +0200

    Update listpostView.php
    use the alternative syntax

--- view/frontend/listpostView.php
+++ view/frontend/listpostView.php
@@ -1,46 +1,40 @@
 <?php $title = 'Mon blog'; ?>
+<?php $tools = new Tools();?>
 <?php ob_start(); ?>
+
 <h1>Par Jean Forteroche</h1>
 <p>Derniers billets du blog :</p>
-<?php
-foreach($posts as $post )
-{
-    $content=htmlspecialchars($post->getContent());
-    if (strlen($content)>100)
-    {
-        $content=substr($content, 0, 100);
-        $dernier_mot=strrpos($content," ");
-        $content=substr($content,0,$dernier_mot);
-    }
-    ?>
+<?php foreach($posts as $post ) :?>
     <div class="news">
         <h3>
-            <a href="index.php?action=post&amp;id=<?= ($post->getId())?>"><?= htmlspecialchars($post->getTitle()) ?></a>
+            <a href="index.php?action=post&amp;id=<?= ($post->getId())?>">
+                <?= htmlspecialchars($post->getTitle()) ?>
+            </a>
             <em><?= ($post->getCreationDate())?></em>
             <?=  "par " . ($post->getAuthor()->getFullname())?>
         </h3>
         <div class="content">
             <p>
-                <?= nl2br($content) ?>
+                <?= $tools->texte_decoupe($content, 100) ?>
             </p>
-                <div class="link">
-                            <div class="row">
-                                <div class="col-8">
-                                </div>
-                                <div class="col-4">
-                                    <a href="index.php?action=post&amp;id=<?= ($post->getId()) ?>">Commentaires</a>
-                                    <a href="index.php?action=post&amp;id=<?= ($post->getId()) ?>"> lire la suite...</a>
-                                </div>
-                            </div>
-               </div>
+            <div class="link">
+                <div class="row">
+                    <div class="col-8">
+                    </div>
+                    <div class="col-4">
+                        <a href="index.php?action=post&amp;id=<?= ($post->getId()) ?>">Commentaires</a>
+                        <a href="index.php?action=post&amp;id=<?= ($post->getId()) ?>"> lire la suite...</a>
+                    </div>
+                </div>
+           </div>
         </div>
     </div>
-<?php
-}
-?>
- <?php for ( $i=1 ; $i <=($nbpages[0]/3+1); $i++)
-     echo"<a href=\"/index.php?page=$i\">page $i</a> ";
-  ?>
+<?php endforeach;?>
+
+ <?php for ( $i=1 ; $i <=($nbpages[0]/3+1); $i++):?>
+     <a href=\"/index.php?page=<?= $i;?>\">page $i</a>
+ <?php endfor?>
+
 <?php $content = ob_get_clean(); ?>
 <?php require('template.php'); ?>
 
